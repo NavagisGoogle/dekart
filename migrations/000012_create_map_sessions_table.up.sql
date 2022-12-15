2facfbea0167ccae9ec61238c71dafcb94ef1105
svillasica-navagis <serge.villasica@navagis.com>
2022-12-15 19:15:30 +0800

    Migration script changes

--- migrations/000012_create_map_sessions_table.up.sql
+++ migrations/000012_create_map_sessions_table.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS map_sessions (
     session_id uuid NOT NULL,
+    map_style text,
     created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
     expiration timestamptz,
     session_token text NOT NULL,
