3677bddd5b9693f98e30ce1f5758dbe0f570442c
svillasica-navagis <serge.villasica@navagis.com>
2022-12-22 12:13:53 +0800

    Changes in map_sessions script

--- migrations/000013_create_map_sessions_table.up.sql
+++ migrations/000013_create_map_sessions_table.up.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS map_sessions (
     map_style text,
     created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
     expiration timestamptz,
-    session_token text NOT NULL,
+    session_token text,
+    tile_input json,
     PRIMARY KEY(session_id)
 );
